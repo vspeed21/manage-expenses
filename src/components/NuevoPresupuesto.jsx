bcafbf8b098574f8a7696f69b3cf0a9d97c285a4
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-25 14:51:00 -0600

    form newPresu done

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import { useState } from 'react';
+
+import useGastos from '../hook/useGastos';
+import Alerta from './Alerta';
 
 function Formulario() {
+  const { presupuesto, setPresupuesto, setIsValidPresupuesto } = useGastos();
+  const [mensaje, setMensaje] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if(!presupuesto || presupuesto <= 0) {
+      setMensaje('presupuesto no valido');
+      setTimeout(() => {
+        setMensaje('')
+      }, 3000);
+      return;
+    }
+    setMensaje('');
+    setIsValidPresupuesto(true);
+  }
+
   return (
-    <form className='bg-white shadow max-w-2xl mx-auto mt-neg p-8'>
+    <form 
+      onSubmit={handleSubmit}
+      className='bg-white shadow max-w-2xl mx-auto mt-neg p-8 contenedor'
+    >
+      {mensaje && (
+        <Alerta
+          mensaje={mensaje}
+          tipo='error'
+        />
+      )}
       <div className='flex flex-col gap-4'>
         <label 
           htmlFor='presupuesto'
           className='text-xl text-blue-500 font-bold'
         >Define tu presupuesto:</label>
         <input
+          type='number'
           id='presupuesto'
           className='w-full border bg-gray-50 focus:outline-0 p-2 text-center text-2xl'
+          value={presupuesto}
+          onChange={e => setPresupuesto(Number(e.target.value))}
         />
       </div>
 
--- src/context/GastosProvider.jsx
+++ src/context/GastosProvider.jsx
@@ -5,12 +5,14 @@ const GastosContext = createContext();
 export function GastosProvider({children}) {
 
   const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidoPrespuesto] = useState(false);
 
   return(
     <GastosContext.Provider
       value={{
         presupuesto,
         setPresupuesto,
+        setIsValidoPrespuesto,
       }}
     >
       {children}
