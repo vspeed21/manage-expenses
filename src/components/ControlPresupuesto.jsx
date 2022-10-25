98a06b1a139a0549656ad4c394108beaf95cd0b9
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-25 16:06:06 -0600

    ControlPresu: classes modified

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,8 +1,35 @@
-
+import useGastos from '../hook/useGastos';
+import { formatearCantidad } from '../helpers';
 
 function ControlPresupuesto() {
+
+  const {presupuesto} = useGastos();
+
   return (
-    <div>ControlPresupuesto</div>
+    <section>
+      <div className="mx-auto bg-white shadow hover:shadow-md transition-shadow py-10 px-5 mt-neg max-w-2xl">
+        <div className="grid md:grid-cols-2 place-items-center justify-center">
+          <p>GRAFICA</p>
+
+          <div className="flex flex-col gap-4">
+            <p className='text-xl'>
+              <span className='font-bold text-blue-600'>Presupuesto: </span>
+              {formatearCantidad(presupuesto)}
+            </p>
+
+            <p className='text-xl'>
+              <span className='font-bold text-blue-600'>Disponible: </span>
+              {formatearCantidad(0)}
+            </p>
+
+            <p className='text-xl'>
+              <span className='font-bold text-blue-600'>Gastado: </span>
+              {formatearCantidad(0)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
