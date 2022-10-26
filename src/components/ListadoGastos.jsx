c46bde57ab791052dab1aa942b829742af204024
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-26 13:57:34 -0600

    ListadoGastos: key added & condition to show Gastos

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -7,11 +7,12 @@ function ListadoGastos() {
   return (
     <>
       <h2 className='text-gray-700 font-bold text-xl mt-10 max-w-2xl mx-auto'>
-        {gastos.lenght > 0 ? 'Gastos' : 'No hay gastos aún'}
+        {gastos.length > 0 ? 'Gastos' : 'No hay gastos aún'}
       </h2>
 
-        {gastos.lenght ? gastos.map(gasto => (
+        {gastos.length ? gastos.map(gasto => (
           <Gasto
+            key={gasto.id}
             gasto={gasto}
           />
         )) : null}
