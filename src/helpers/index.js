63da9e11b2c1a0a26354f2c4501745c7aa0c89be
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-25 22:39:23 -0600

    helpers: formatearFecha created

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,3 +4,13 @@ export function formatearCantidad(cantidad) {
     currency: 'USD'
   });
 }
+
+export function formatearFecha(fecha){
+  const fechaNueva = new Date(fecha);
+
+  return fechaNueva.toLocaleDateString('es-ES', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  });
+}
