616d62ae7854ad0011a43d1999059b8d7737e3af
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-26 16:26:20 -0600

    Gasto: swipeable items added

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,3 +1,12 @@
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
+
 import { formatearFecha } from '../helpers';
 import IconoAhorro from '../img/icono_ahorro.svg';
 import IconoCasa from '../img/icono_casa.svg';
@@ -21,29 +30,52 @@ const diccionarioIconos = {
 function Gasto({gasto}) {
   const {nombre, cantidad, categoria, id, fecha} = gasto
 
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={() => console.log('editando')}>
+        Editar
+      </SwipeAction>
+    </LeadingActions>
+  )
+
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction onClick={() => console.log('eliminando...')}>
+        Eliminar
+      </SwipeAction>
+    </TrailingActions>
+  )
+
   return (
-    <div className='bg-white shadow p-5 rounded max-w-2xl mt-5 mx-auto flex justify-between items-center mb-5'>
-      <div className='flex gap-5 items-center'>
-        <img
-          src={diccionarioIconos[categoria]}
-          alt={`imagen categoria ${nombre}`}
-          className='w-20'
-        />
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+      >
+        <div className='bg-white shadow p-5 rounded mx-auto flex justify-between items-center mb-5 w-full max-w-2xl'>
+          <div className='flex gap-5 items-center'>
+            <img
+              src={diccionarioIconos[categoria]}
+              alt={`imagen categoria ${nombre}`}
+              className='w-20'
+            />
 
-        <div className='space-y-1'>
-          <p className='text-xl'>{nombre}</p>
-          <p className='capitalize'>{categoria === 'gastos' ? 'Gastos varios' : categoria}</p>
-          <p className='text-gray-600'>
-            Agregado el: {''}
-            <span className='font-bold'>
-              {formatearFecha(fecha)}
-            </span>
-          </p>
+            <div className='space-y-1'>
+              <p className='capitalize text-gray-600 font-bold'>{categoria === 'gastos' ? 'Gastos varios' : categoria}</p>
+              <p className='text-xl'>{nombre}</p>
+              <p className='text-gray-600'>
+                Agregado el: {''}
+                <span className='font-bold'>
+                  {formatearFecha(fecha)}
+                </span>
+              </p>
+            </div>
+          </div>
+
+          <p className='font-bold text-xl'>${cantidad}</p>
         </div>
-      </div>
-
-      <p className='font-bold text-xl'>${cantidad}</p>
-    </div>
+      </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
