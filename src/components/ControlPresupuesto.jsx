a49b131ab9a0ebbd6808cdfd4139d87eefc4cdb0
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-26 22:00:44 -0600

    ControlPresu: Grafica progressBar added

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -5,23 +5,46 @@ import Modal from './Modal';
 import ListadoGastos from './ListadoGastos';
 import { useEffect, useState } from 'react';
 
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css'
+
 function ControlPresupuesto() {
 
   const [disponible, setDisponible] = useState(0);
   const [gastado, setGastado] = useState(0);
   const {presupuesto, showModal, modal, gastos} = useGastos();
 
+  const [porcentaje, setPorcentaje] = useState(0);
+
   useEffect( () => {
     const cantidadGastada = gastos.reduce((acc, gasto) => gasto.cantidad + acc, 0);
     setGastado(cantidadGastada);
     setDisponible(presupuesto - cantidadGastada);
+
+    const porcentajeNuevo = ((( presupuesto - (presupuesto - cantidadGastada) ) /presupuesto )*100).toFixed(2);
+
+    setTimeout(() => {
+      setPorcentaje(porcentajeNuevo);
+    }, 1000);
+
   }, [gastos]);
 
   return (
     <section>
       <div className="mx-auto bg-white shadow hover:shadow-md transition-shadow py-10 px-5 mt-neg max-w-2xl">
         <div className="grid md:grid-cols-2 place-items-center justify-center">
-          <p>GRAFICA</p>
+          <div className="w-56 h-56">
+            <CircularProgressbar
+              value={porcentaje}
+              text={`${porcentaje}% gastado`}
+              styles={buildStyles({
+                pathColor: '#2564eb',
+                trailColor: '#F5F5F5',
+                textSize: '10px',
+                textColor: '#2564eb'
+              })}
+            />
+          </div>
 
           <div className="flex flex-col gap-4">
             <p className='text-xl'>
