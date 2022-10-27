93a1de32c54a367aed102beee0f94ae2833986a9
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-26 23:30:00 -0600

    ListadoGastos: condition to show gastosFiltrados & gastos

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,21 +1,44 @@
 import useGastos from '../hook/useGastos'
+import Filtros from './Filtros'
 import Gasto from './Gasto'
 
 function ListadoGastos() {
-  const { gastos } = useGastos()
+  const { gastos, filtro, gastosFiltrados} = useGastos()
 
   return (
     <>
-      <h2 className='text-gray-700 font-bold text-xl mt-10 max-w-2xl mx-auto'>
-        {gastos.length > 0 ? 'Gastos' : 'No hay gastos aún'}
-      </h2>
+      <Filtros/>
 
-        {gastos.length ? gastos.map(gasto => (
-          <Gasto
-            key={gasto.id}
-            gasto={gasto}
-          />
-        )) : null}
+      {
+        filtro ? (
+          <>
+            <h2 className='text-gray-700 font-bold text-xl mt-10 max-w-2xl mx-auto py-5'>
+              {gastosFiltrados.length > 0 ? 'Gastos' : 'No hay gastos en esta categoría'}
+            </h2>
+
+            {gastosFiltrados.length && gastosFiltrados.map(gasto => (
+              <Gasto
+                key={gasto.id}
+                gasto={gasto}
+              />
+            ))}
+          </>
+
+        ):(
+          <>
+            <h2 className='text-gray-700 font-bold text-xl mt-10 max-w-2xl mx-auto py-5'>
+              {gastos.length > 0 ? 'Gastos' : 'No hay gastos aún. Añade uno'}
+            </h2>
+
+            {gastos.length && gastos.map(gasto => (
+              <Gasto
+                key={gasto.id}
+                gasto={gasto}
+              />
+            ))}
+          </>
+        )
+      }
     </>
   )
 }
