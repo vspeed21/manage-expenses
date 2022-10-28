0e6d2fa52176c180ae58768521f3694669422405
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-28 16:06:14 -0600

    ListadoGastos: tutorial added to tell user how work my app

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
+
 import useGastos from '../hook/useGastos'
 import Filtros from './Filtros'
 import Gasto from './Gasto'
 
 function ListadoGastos() {
-  const { gastos, filtro, gastosFiltrados} = useGastos()
+  const { gastos, filtro, gastosFiltrados} = useGastos();
+
+  const [siguiente, setSiguiente] = useState('editar');
 
   return (
     <div className='w-11/12 mx-auto'>
@@ -30,11 +34,51 @@ function ListadoGastos() {
               {gastos.length > 0 ? 'Gastos' : 'No hay gastos aún. Añade uno'}
             </h2>
 
-            {gastos.length && gastos.map(gasto => (
-              <Gasto
-                key={gasto.id}
-                gasto={gasto}
-              />
+            <div 
+              className={`relative mb-6 triangulo bg-blue-300 p-2 rounded-md max-w-max mx-auto transition-opacity duration-300 
+              ${gastos.length === 1 && siguiente === 'editar' ? 'block' : 'hidden'}
+              
+              `}
+            >
+                <p className='text-lg'>Para modificar un gasto desliza hacia la derecha</p>
+              <div className='flex justify-end gap-3 mt-1'>
+                <button
+                  type='button'
+                  className=''
+                  onClick={() => setSiguiente('')}
+                >
+                  Saltar tutorial
+                </button>
+
+                <button
+                  type='button'
+                  className='font-bold'
+                  onClick={() => setSiguiente('eliminar')}
+                >
+                  Siguiente
+                </button>
+              </div>
+            </div>
+
+            <div className={`relative mb-6 triangulo bg-blue-300 p-2 rounded-md max-w-max mx-auto transition-opacity duration-300 ${siguiente === 'eliminar' ? 'block': 'hidden'}`}>
+              <p className='text-lg'>Para eliminar un gasto desliza hacia la izquierda</p>
+
+              <div className='flex justify-end'>
+                <button
+                  type='button'
+                  className='font-bold'
+                  onClick={() => setSiguiente('')}
+                >
+                  Cerrar
+                </button>
+              </div>
+            </div>
+
+          {gastos.length && gastos.map(gasto => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+            />
             ))}
           </>
         )
